7c07e57359b846ae60367d2186630bde7a62f2f6
Vishal Chauhan <chauhanvishal00001@gmail.com>
2025-07-13 21:30:06 +0530

    first

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/info")
+def lwinfo():
+       return "i m Vishal from India"
+
+@app.route("/phone")
+def lwphone():
+       return "9306777761"
+
+app.run(host="0.0.0.0")
